f1718151b467570635b0dbf7180f46ecebc99e11
Tristan <kraig.tri125@gmail.com>
2015-10-22 22:55:36 -0400

    UserControl FenetreInfoMembre pour FenetreGestionAdmin
    FenetreInfoMembre affichera les informations détailés d'un membre sélectionné
    dans la FenetreGestionAdmin.
    
    Contient un TextBlock pour afficher les informations.
    Expose au binding la propriété MembreContent pour obtenir un objet Membre
    source.

--- UI/Views/FenetreInfoMembre.xaml.cs
+++ UI/Views/FenetreInfoMembre.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,44 @@ namespace Nutritia.UI.Views
     /// <summary>
     /// Interaction logic for FenetreInfoMembre.xaml
     /// </summary>
-    public partial class FenetreInfoMembre : UserControl
+    public partial class FenetreInfoMembre : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Membre membre { get; set; }
+
         public FenetreInfoMembre()
         {
             InitializeComponent();
+
+        }
+
+        public Object MembreContent
+        {
+            get { return (Object)GetValue(MembreProperty); }
+            set
+            {
+                SetValue(MembreProperty, value);
+                NotifyPropertyChanged("MembreContent");
+            }
+        }
+
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public static readonly DependencyProperty MembreProperty = DependencyProperty.Register("MembreContent", typeof(Membre), typeof(FenetreInfoMembre), new FrameworkPropertyMetadata(null));
+
+
+
+        private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Membre m = MembreContent as Membre;
+            Console.WriteLine("");
         }
     }
 }
