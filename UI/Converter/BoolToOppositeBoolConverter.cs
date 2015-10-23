e5bd2ab4f68406ec648014ddfec3b0c94aa4806f
Tristan <kraig.tri125@gmail.com>
2015-10-22 22:46:03 -0400

    Rajout d'un convertiseur qui donne la valeur inverse d'un bool. Pour faire du Binding.

---
+++ UI/Converter/BoolToOppositeBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Nutritia.UI.Converter
+{
+    //http://www.codeproject.com/Articles/24330/WPF-Bind-to-Opposite-Boolean-Value-Using-a-Convert
+    public class BoolToOppositeBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
