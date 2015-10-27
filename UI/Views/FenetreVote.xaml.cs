942afbcc3254d38bdc75a613910e5f615038be84
glesaux <glesaux@live.ca>
2015-10-27 11:13:14 -0400

    Liaison du popup de votes avec la fenêtre de votes

--- UI/Views/FenetreVote.xaml.cs
+++ UI/Views/FenetreVote.xaml.cs
@@ -19,9 +19,32 @@ namespace Nutritia.UI.Views
     /// </summary>
     public partial class FenetreVote : Window
     {
+        /// <summary>
+        /// Constructeur par défaut de la classe.
+        /// </summary>
         public FenetreVote()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Événement lancé sur un clique du bouton Confirmer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Événement lancé sur un clique du bouton Confirmer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConfirmer_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Fonctionnalité à venir dans la version 1.");
         }
     }
 }
