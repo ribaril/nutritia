2b2d3e1a3ec9a3677f2dcb0684970e400ebffb75
Tristan <kraig.tri125@gmail.com>
2015-10-30 11:29:55 -0400

    Rajout de la classe QrCodeHelper pour aider l'utilisation de QrCode.Net

---
+++ Toolkit/QrCodeHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using Gma.QrCodeNet.Encoding;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
+
+namespace Nutritia.Toolkit
+{
+    public static class QrCodeHelper
+    {
+
+        public static BitmapImage getQrBitmap(string text, ErrorCorrectionLevel errorCorrection = ErrorCorrectionLevel.M)
+        {
+            QrCode qrCode;
+            QrEncoder qrEncoder = new QrEncoder(errorCorrection);
+            qrEncoder.TryEncode(text, out qrCode);
+
+            GraphicsRenderer gRenderer = new GraphicsRenderer(
+                new FixedModuleSize(2, QuietZoneModules.Two),
+                Brushes.Black, Brushes.White);
+            BitmapImage bi = new BitmapImage();
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                gRenderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, memoryStream);
+                bi.BeginInit();
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.StreamSource = memoryStream;
+                bi.EndInit();
+            }
+            return bi;
+        }
+
+    }
+}
