c12ef8e2f1708246c5a7f627db8682f61f274960
Tristan <kraig.tri125@gmail.com>
2015-12-15 22:25:16 -0500

    Commentaire QrCodeHelper et BoolToOppositeBoolConverter

--- Toolkit/QrCodeHelper.cs
+++ Toolkit/QrCodeHelper.cs
@@ -12,21 +12,32 @@ using System.Windows.Media.Imaging;
 
 namespace Nutritia.Toolkit
 {
+    /// <summary>
+    /// Classe statique haut niveau aidant l'utilisation de la librairie QrCode.Net
+    /// </summary>
     public static class QrCodeHelper
     {
-
+        /// <summary>
+        /// Méthode retournant un objet d'image Bitmap d'un code QR
+        /// </summary>
+        /// <param name="text">Le texte à encoder</param>
+        /// <param name="errorCorrection">Le niveau de correction d'erreur choisi. Détermine le pourcentage de la structure du codeQR est utilisé pour corriger les erreurs.</param>
+        /// <returns>Objet BitmapImage du codeQR</returns>
         public static BitmapImage getQrBitmap(string text, ErrorCorrectionLevel errorCorrection = ErrorCorrectionLevel.M)
         {
             QrCode qrCode;
             QrEncoder qrEncoder = new QrEncoder(errorCorrection);
+            //Peut techniquement échouer et le cas n'est pas géré.
             qrEncoder.TryEncode(text, out qrCode);
 
+            //Détermine l'épaisseur des traits, la couleur de fond et la couleur du code QR.
             GraphicsRenderer gRenderer = new GraphicsRenderer(
                 new FixedModuleSize(2, QuietZoneModules.Two),
                 Brushes.Black, Brushes.White);
             BitmapImage bi = new BitmapImage();
             using (MemoryStream memoryStream = new MemoryStream())
             {
+                //Utilise les bytes du codeQR encodé qui se trouve dans l'objet qrCode dans un MemoryStream pour faire une BitmapImage.
                 gRenderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, memoryStream);
                 bi.BeginInit();
                 bi.CacheOption = BitmapCacheOption.OnLoad;
--- UI/Converter/BoolToOppositeBoolConverter.cs
+++ UI/Converter/BoolToOppositeBoolConverter.cs
@@ -9,6 +9,10 @@ using System.Windows.Data;
 namespace Nutritia.UI.Converter
 {
     //http://www.codeproject.com/Articles/24330/WPF-Bind-to-Opposite-Boolean-Value-Using-a-Convert
+    /// <summary>
+    /// Class pour faire du binding avec la valeur opposé d'un bool
+    /// Utilisé pour désactiver le checkBox d'un compte membre bannis dans la fenêtre des droits d'administration
+    /// </summary>
     public class BoolToOppositeBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
